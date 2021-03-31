e45f04320338862aa261537f8e645ad9687e95a6
bahp <bernard.hernandez.perez@gmail.com>
2021-03-31 22:40:21 +0100

    ~ new plotly treemap examples

---
+++ examples/visualization/plot_pyplot_sunburst.py
@@ -0,0 +1,53 @@
+"""
+Plotly - Sunburst (sari)
+------------------------
+
+.. warning:: It might take some time to load.
+
+"""
+
+# Plotly
+import plotly.express as px
+
+# Import own libraries
+from pyamr.datasets.load import load_data_nhs
+
+
+# --------------------------------------------------------------------
+#                               Main
+# --------------------------------------------------------------------
+# Load data
+data, antibiotics, organisms = load_data_nhs(nrows=1000)
+
+# Create DataFrame
+dataframe = data.groupby(['specimen_code',
+                          'microorganism_code',
+                          'antimicrobial_code',
+                          'sensitivity']) \
+                .size().unstack().fillna(0)
+
+# Compute frequency
+dataframe['freq'] = dataframe.sum(axis=1)
+
+# Compute sari
+dataframe['sari'] = dataframe.resistant / \
+    (dataframe.resistant + dataframe.sensitive)
+
+# Reset index
+dataframe = dataframe.reset_index()
+
+# -------------------------------------------
+# Plot
+# -------------------------------------------
+# Show
+fig = px.sunburst(dataframe,
+    path=['specimen_code',
+          'microorganism_code',
+          'antimicrobial_code'],
+    values='freq',
+    color='sari',
+    title='Sunburst of <Microorganisms, Antimicrobials> pairs')
+
+# Show
+#fig.show()
+fig
